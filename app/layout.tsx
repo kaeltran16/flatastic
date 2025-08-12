b7b1dba0efa1b6c8ae862aa2433219e1c5ba6c6d
Kael <khang.tran@mozox.com>
2025-08-12 14:51:17 +0700

    add analytics and insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { AppBreadcrumb } from '@/components/app-breadcrumb';
 import { Navbar } from '@/components/navbar';
 import { Toaster } from '@/components/ui/sonner';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -61,6 +63,8 @@ export default function RootLayout({
           <AppBreadcrumb />
           <main>{children}</main>
           <Toaster />
+          <Analytics />
+          <SpeedInsights />
         </div>
       </body>
     </html>
